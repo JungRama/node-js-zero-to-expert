62ee474a359c145d79a15a13394acadaf79b2590
Jung Rama <jungrama.id@gmail.com>
2020-03-24 13:59:09 +0800

    finishing monggose

--- 7.nosql-mongoose/controller/frontend/orderController.js
+++ 7.nosql-mongoose/controller/frontend/orderController.js
@@ -1,14 +1,28 @@
+const Order = require('../../models/order')
+
 exports.addOrder = ( req, res, next ) => {
-    req.user.addOrder()
-    .then(() => {
+    const order = new Order({
+        items: req.user.cart.items,
+        userId: req.user._id
+    })
+    
+    order.save()
+    .then(result => {
+        req.user.cart.items = []
+        req.user.save()
         res.redirect('/cart')
+    })
+    .catch(err => {
+        console.log(err);
     })
 }
 
 exports.getOrder = (req, res, next) => {
-    req.user.getOrder()
+    Order.find()
+    .populate('items.productId')
+    .where('userId').equals(req.user._id)
     .then(orders => {
-        // console.log({o : orders});
+        console.log({o : orders});
         
         res.render('frontend/order', {
             rName: 'frontOrder',
--- 7.nosql-mongoose/models/order.js
+++ 7.nosql-mongoose/models/order.js
@@ -1,13 +1,24 @@
-const Sequelize = require('sequelize')
-const db = require('../helpers/database')
+const mongoose = require('mongoose')
 
-const Order = db.define('order', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+const Scheme = mongoose.Schema
+
+const orderScheme = new Scheme({
+    items: [{
+        productId: {
+            type: Scheme.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }],
+    userId: {
+        type: Scheme.Types.ObjectId,
+        required: true,
+        ref: 'User',
     }
 })
 
-module.exports = Order
+module.exports = mongoose.model('Order', orderScheme)
