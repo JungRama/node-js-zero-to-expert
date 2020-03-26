14a0c2cc527c47d56ddd4f119b6e92d6b372b705
Jung Rama <jungrama.id@gmail.com>
2020-03-26 17:40:35 +0800

    finishing authentication

--- 8.basic-authentication/controller/authController.js
+++ 8.basic-authentication/controller/authController.js
@@ -1,13 +1,97 @@
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         rName: 'authLogin',
         title: 'Login',
+        message: req.flash('message')
     });
 };
+
+exports.postLogin = (req, res, next) => {
+    const request = req.body
+
+    User.findOne({email: request.email})
+    .then(user => {
+        if(!user){
+            req.flash('message', {
+                type: 'is-danger',
+                text: 'Invalid email or password'
+            })
+            res.redirect('/login')
+        }else{
+            bcrypt.compare(request.password, user.password)
+            .then((match) =>{
+                if(match){
+                    req.session.user = user._id;
+                    req.session.isLoggedIn = true
+                    res.redirect('/')
+                }else{
+                    req.flash('message', {
+                        type: 'is-danger',
+                        text: 'Invalid email or password'
+                    })
+                    res.redirect('/login')
+                }
+            })
+            .catch(err => {
+                console.log('failed to bcyrpt ' + err);
+            })
+        }
+    })
+}
 
 exports.getRegister = (req, res, next) => {
     res.render('auth/register', {
         rName: 'authRegister',
         title: 'Register',
+        message: req.flash('message'),
     });
 };
+
+exports.postRegister = (req, res, next) => {
+    const request = req.body
+
+    if(request.password != request.confirm_password){
+        req.flash('message', {
+            type: 'is-danger',
+            text: 'Password must be match'
+        })
+        res.redirect('/register')
+    }else{
+        
+        bcrypt.hash(request.password, 12)
+        .then(password => {
+
+            const user = new User({
+                name: request.name,
+                email: request.email,
+                password: password,
+                cart: {
+                    items: []
+                }
+            })
+
+            user.save()
+            .then(response => {
+                req.flash('message', {
+                    type: 'is-success',
+                    text: 'Success create account, Login to continue'
+                })
+                res.redirect('/login')
+            })
+            .catch(err => {
+                req.flash('message', {
+                    type: 'is-danger',
+                    text: 'Internal Server Error : ' + err
+                })
+                res.redirect('/register')
+            })
+
+        }).catch(err => {
+            console.log('failed to bcyrpt ' + err);
+        })
+
+    }
+}
--- 8.basic-authentication/routes/frontend.js
+++ 8.basic-authentication/routes/frontend.js
@@ -1,18 +1,20 @@
 const express   = require('express')
 const router    = express.Router()
 
-// CONTROLLER
+/* ------------------------------- MIDDLEWARE ------------------------------- */
+const authMiddleware = require('../middleware/auth')
+/* ------------------------------- CONTROLLER ------------------------------- */
 const productController = require('../controller/productsController.js')
 const cartController = require('../controller/cartController.js')
 const orderController = require('../controller/orderController.js')
 
 router.get('/', productController.getAllProduct)
 
-router.get('/cart', cartController.getCart)
-router.post('/cart', cartController.addCart)
-router.post('/cart/delete', cartController.deleteCart)
+router.get('/cart', authMiddleware, cartController.getCart)
+router.post('/cart', authMiddleware, cartController.addCart)
+router.post('/cart/delete', authMiddleware, cartController.deleteCart)
 
-router.get('/order', orderController.getOrder)
-router.post('/order', orderController.addOrder)
+router.get('/order', authMiddleware, orderController.getOrder)
+router.post('/order', authMiddleware, orderController.addOrder)
 
 module.exports = router
